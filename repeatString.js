4a93b04abfdf8d6e8a19fa6d7e5cd7da87822b54
meen- yaduvanshi <meenuyaduvanshi24@gmail.com>
2023-09-05 16:52:50 +0530

    make some changes

---
+++ repeatString.js
@@ -0,0 +1,18 @@
+
+function repeat(str, num) {
+    var output = "";
+    if (num > 0){
+        for (var i = 0; i < num; i++) {
+            output = output + str;
+        }
+    console.log(output);
+}
+else {
+    console.log("error");
+}
+}
+
+repeat("Meenu" , 4)
+repeat("Ping" , 1)
+repeat("Ping" , 0)
+repeat("Hey" , 2)
